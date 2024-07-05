1dae1934cc1f1873a337f57ce20b1b2edb9bfe29
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2024-07-05 16:19:26 +0530

    not completed yet

---
+++ app.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icons = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icons.classList.toggle("open");
+}
